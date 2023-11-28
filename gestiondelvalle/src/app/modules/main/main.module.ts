73a0987b13e39aeffdaa8d43150627ba51d96a61
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 02:34:35 -0300

    se termina main

--- gestiondelvalle/src/app/modules/main/main.module.ts
+++ gestiondelvalle/src/app/modules/main/main.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarritoComponent } from './pages/carrito/carrito.component';
+import { MainComponent } from './pages/main/main.component';
+import { ProductoseleccionadoComponent } from './pages/productoseleccionado/productoseleccionado.component';
 
 
 
 @NgModule({
   declarations: [
-    CarritoComponent
+    CarritoComponent,
+    MainComponent,
+    ProductoseleccionadoComponent
   ],
   imports: [
     CommonModule
---
+++ gestiondelvalle/src/app/modules/main/pages/productoseleccionado/productoseleccionado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productoseleccionado',
+  templateUrl: './productoseleccionado.component.html',
+  styleUrls: ['./productoseleccionado.component.css']
+})
+export class ProductoseleccionadoComponent {
+
+}

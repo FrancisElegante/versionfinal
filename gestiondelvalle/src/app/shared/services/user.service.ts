11869c132473675a4c3bef4a129c2dd88957d356
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 20:43:18 -0300

    Se sigue con los imports

--- gestiondelvalle/src/app/shared/services/user.service.ts
+++ gestiondelvalle/src/app/shared/services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import { User } from "../../models/user.interface";
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'

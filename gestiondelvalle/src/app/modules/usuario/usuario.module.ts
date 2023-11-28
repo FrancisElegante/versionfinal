ad67fe382ab2b0e672fc2a961b1a2f3eb85d76a8
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 05:46:22 -0300

    se encuentra un erorr y se guarda para resolver

--- gestiondelvalle/src/app/modules/usuario/usuario.module.ts
+++ gestiondelvalle/src/app/modules/usuario/usuario.module.ts
@@ -4,8 +4,21 @@ import { MainComponent } from './pages/usuario/main.component';
 import { ContrasenaComponent } from './pages/contrasena/contrasena.component';
 import { DatosComponent } from './pages/datos/datos.component';
 import { DireccionComponent } from './pages/direccion/direccion.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: 'contrasena', component: ContrasenaComponent},
+      {path: 'datos', component: DatosComponent},
+      {path: 'direccion', component: DireccionComponent},
+      {path: 'direccion', component: usu},
 
+    ]
+  }
+
+]
 
 @NgModule({
   declarations: [
@@ -15,7 +28,7 @@ import { DireccionComponent } from './pages/direccion/direccion.component';
     DireccionComponent
   ],
   imports: [
-    CommonModule
+    RouterModule.forChild( routes )
   ]
 })
 export class MainModule { }

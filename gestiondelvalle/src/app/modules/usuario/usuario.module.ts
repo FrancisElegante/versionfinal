42f97aa0be9975802f0a0aeec3385964bfef0558
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 02:30:52 -0300

    se cambia main por usuario jaja

--- gestiondelvalle/src/app/modules/usuario/usuario.module.ts
+++ gestiondelvalle/src/app/modules/usuario/usuario.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './pages/main/main.component';
+import { MainComponent } from './pages/usuario/main.component';
 import { ContrasenaComponent } from './pages/contrasena/contrasena.component';
 import { DatosComponent } from './pages/datos/datos.component';
 import { DireccionComponent } from './pages/direccion/direccion.component';

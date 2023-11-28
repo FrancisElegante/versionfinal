e039107aec2c224c859ee370bd31450773b8baeb
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 02:13:28 -0300

    soluciono el error del path por haber cambiado rutas

--- gestiondelvalle/src/app/app-routing.module.ts
+++ gestiondelvalle/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path:'auth',
-    loadChildren:()=> import('./auth/auth.module').then(m=>m.AuthModule)
+    loadChildren:()=> import('./modules/auth/auth.module').then(m=>m.AuthModule)
   }
 ];
 

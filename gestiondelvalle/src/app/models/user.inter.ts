a6830ef5d3a7db40fed6712ae841ac356c9f7250
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 20:39:55 -0300

    se hace la interfaz

---
+++ gestiondelvalle/src/app/models/user.inter.ts
@@ -0,0 +1,12 @@
+export interface User{
+    uid: string;
+    nombre: string;
+    apellido: string;
+    sexo: string;
+    edad: number;
+    correo: string;
+    contraseña: string;
+    contraseña2: string;
+    rol: string ;
+    imagen: string;
+}

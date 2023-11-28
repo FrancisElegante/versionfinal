5335a4dae7e54b138baf511388ad424cefef1738
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 07:25:00 -0300

    se instala firebase

--- gestiondelvalle/src/app/app.module.ts
+++ gestiondelvalle/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"prototipo-1315d","appId":"1:693325482804:web:f76329ba76b0a2e2b33361","storageBucket":"prototipo-1315d.appspot.com","locationId":"us-central","apiKey":"AIzaSyB2D1Xf4RB-dkF1_HdImRmdhlqB2jpokq4","authDomain":"prototipo-1315d.firebaseapp.com","messagingSenderId":"693325482804"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]

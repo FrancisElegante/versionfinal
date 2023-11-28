9e290a9e16acd23678afffd47fb3043f45c386c8
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 02:33:05 -0300

    se agrega main

---
+++ gestiondelvalle/src/app/modules/main/main.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarritoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }

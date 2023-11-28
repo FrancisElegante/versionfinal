d60ff333322192a245e45ea13b4704393bfcee93
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-27 23:56:35 -0300

    septimo commit, rutas hijas creadas, testeamos

--- gestiondelvalle/src/app/auth/authrouting.module.ts
+++ gestiondelvalle/src/app/auth/authrouting.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
+    ]
+  }
+
+]
 
 
 @NgModule({

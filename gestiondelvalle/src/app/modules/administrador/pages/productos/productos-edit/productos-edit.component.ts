e6c2da31871f97002e3d7b08c6250ae362cdada9
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 08:30:35 -0300

    dikasjdbac

--- gestiondelvalle/src/app/modules/administrador/pages/productos/productos-edit/productos-edit.component.ts
+++ gestiondelvalle/src/app/modules/administrador/pages/productos/productos-edit/productos-edit.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductosService } from 'src/app/shared/services/productos.service';
+import { Productos } from 'src/app/models/productos.interface';
+
 
 @Component({
   selector: 'app-productos-edit',
   templateUrl: './productos-edit.component.html',
   styleUrls: ['./productos-edit.component.css']
 })
-export class ProductosEditComponent {
+export class ProductosEditComponent implements OnInit{
+  form: FormGroup;
+  producto: Productos | null = null;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private productosService: ProductosService
+  ) {
+    this.form = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      tipo: ['', Validators.required],
+      precio: ['', Validators.required],
+      image: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.producto = history.state.productos; // Corregir el nombre de la propiedad
+
+    // Inicializa el formulario con las validaciones
+    this.form = this.formBuilder.group({
+      nombre: [this.producto?.nombre || '', Validators.required],
+      descripcion: [this.producto?.descripcion || '', Validators.required],
+      tipo: [this.producto?.tipo || '', Validators.required],
+      precio: [this.producto?.precio || '', Validators.required],
+      image: [this.producto?.image || '', Validators.required]
+    });
+  }
+  
+
+  updateProducto(): void {
+    if (this.form.invalid || !this.producto) {
+      return;
+      console.log("ERROR")
+    }
+  
+    const updatedProduct: Productos = {
+      id: this.producto.id,
+      ...this.form.value
+    };
+
+    this.productosService.updatePlayer(updatedProduct)
+      .then(() => {
+        console.log('Producto actualizado');
+        this.router.navigateByUrl('/administrador/productos');
+      })
+      .catch((error) => {
+        console.error('Error al actualizar el producto', error);
+      });
+  }
 }
--- gestiondelvalle/src/app/modules/administrador/pages/productos/productos.component.ts
+++ gestiondelvalle/src/app/modules/administrador/pages/productos/productos.component.ts
@@ -4,11 +4,45 @@ import { debounceTime, Observable } from 'rxjs';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { ProductosService } from 'src/app/shared/services/productos.service';
+import { Productos } from 'src/app/models/productos.interface';
+
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  productos$!: Observable<Productos[]>;
+  _productosService = inject(ProductosService);
+  _router = inject(Router);
+  searcher = new FormControl('');
 
+  ngOnInit(): void {
+    this.productos$ = this._productosService.getPlayers();
+    this.searcher.valueChanges.pipe(debounceTime(1000)).subscribe((search) => {
+      // this._playerService.
+      if (search) {
+        console.log(search);
+        this.productos$ = this._productosService.getPlayers(search);
+      } else {
+        this.productos$ = this._productosService.getPlayers();
+      }
+    });
+  }
+
+  editPlayer(productos: Productos) {
+    this._router.navigateByUrl('/administrador/productos-edit', { state: { productos } });
+    console.log(productos)
+    console.log(this.productos$)
+  }
+
+  deletePlayer(productos: Productos) {
+    if (confirm(`Seguro de borrar a ${productos.nombre}`)) {
+      if (productos.id) { // Verificar que el ID esté definido
+        this._productosService.deletePlayer(productos.id);
+      }
+    }
+  }
+  
 }

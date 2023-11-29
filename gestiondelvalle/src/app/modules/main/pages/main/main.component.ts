0e04826f8dc994e4c54e512d8ab4e2b7304e18da
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 02:43:39 -0300

    se inicia productos

--- gestiondelvalle/src/app/modules/main/pages/main/main.component.ts
+++ gestiondelvalle/src/app/modules/main/pages/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Firestore, collection, addDoc, collectionData, doc, deleteDoc, setDoc, getDoc, DocumentReference } from '@angular/fire/firestore';
+import { User as Userinterface } from "../../../../models/user.interface";
 
 @Component({
   selector: 'app-main',
--- gestiondelvalle/src/app/shared/services/productos.service.ts
+++ gestiondelvalle/src/app/shared/services/productos.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import {
+  getDoc,
+  query,
+  updateDoc,
+  where,
+} from '@angular/fire/firestore';
+
+import { getDocs } from '@firebase/firestore';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Productos } from '../../models/productos.interface';
+import {  Firestore, collection, addDoc, collectionData, doc, deleteDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'

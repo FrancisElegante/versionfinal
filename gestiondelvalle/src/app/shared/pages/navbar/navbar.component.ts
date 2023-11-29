81e3a8f2e6a36f18e51597db109dc64042efd25e
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 01:38:29 -0300

    se continua navbarback

--- gestiondelvalle/src/app/shared/pages/navbar/navbar.component.ts
+++ gestiondelvalle/src/app/shared/pages/navbar/navbar.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { User } from 'firebase/auth';
+import { Router } from '@angular/router';
+import { Firestore} from '@angular/fire/firestore';
+import { AfterViewInit, ElementRef, OnInit, inject } from '@angular/core';
 import { UserService } from '../../../shared/services/user.service';
-import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -8,44 +12,20 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
 
-  formReg: FormGroup;
+export class NavbarComponent {
 
-  constructor(private userService: UserService) {
-    this.formReg = new FormGroup({
-      nombre: new FormControl(),
-      rol: new FormControl('comprador'),
-      apellido: new FormControl(),
-      sexo: new FormControl(),
-      edad: new FormControl(),
-      correo: new FormControl(),
-      imagen: new FormControl(),
-      contraseña: new FormControl(),
-      contraseña2: new FormControl()
-    });
-  }
+  user: User | null = null; // Initialize with null
+  login: boolean = false;
+  rol: 'comprador' | 'admin' = 'comprador';
 
-  ngOnInit(): void {
-  }
 
-  onSubmit(): void {
-    this.userService.register(this.formReg.value)
-      .then(response => {
-        console.log(response);
-        console.log("Éxito al crear usuario");
-        const uid = response.user.uid;
-        this.userService.guardarDatos(uid, this.formReg.value)
-          .then(() => {
-            console.log("Datos guardados en Firestore");
-          })
-          .catch(error => {
-            console.log("Error al guardar los datos en Firestore:", error);
-          });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+
+
+
+
+
+
+
 
 }

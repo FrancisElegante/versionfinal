da7f7feaa4e46392df95ca6bc1b3bbd991d49162
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 07:47:37 -0300

    primer commit admini

--- gestiondelvalle/src/app/modules/administrador/pages/productos/productos.component.ts
+++ gestiondelvalle/src/app/modules/administrador/pages/productos/productos.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { debounceTime, Observable } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-productos',

a70a4df0aed45943e7d1041e1bc333c63afb48e0
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 21:51:39 -0300

    se comienza el backend del login

--- gestiondelvalle/src/app/modules/auth/pages/login/login.component.ts
+++ gestiondelvalle/src/app/modules/auth/pages/login/login.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../../../shared/services/user.service';
+import { Component, OnInit, inject } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  _router = inject(Router);
+
+  formLogin: FormGroup;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+    
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+
 
 }

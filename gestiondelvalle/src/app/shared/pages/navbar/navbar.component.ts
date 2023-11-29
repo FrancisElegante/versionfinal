7cc5f0311459d4a2d998af038d45306e0b516ce5
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 01:35:27 -0300

    se crea el back de la navbar

--- gestiondelvalle/src/app/shared/pages/navbar/navbar.component.ts
+++ gestiondelvalle/src/app/shared/pages/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../shared/services/user.service';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  formReg: FormGroup;
+
+  constructor(private userService: UserService) {
+    this.formReg = new FormGroup({
+      nombre: new FormControl(),
+      rol: new FormControl('comprador'),
+      apellido: new FormControl(),
+      sexo: new FormControl(),
+      edad: new FormControl(),
+      correo: new FormControl(),
+      imagen: new FormControl(),
+      contraseña: new FormControl(),
+      contraseña2: new FormControl()
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  
 }

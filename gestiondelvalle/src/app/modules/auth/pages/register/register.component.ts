c34f123a151e732f990e01065f6ad7639ca07dd4
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 01:27:09 -0300

    se va finalizando register

--- gestiondelvalle/src/app/modules/auth/pages/register/register.component.ts
+++ gestiondelvalle/src/app/modules/auth/pages/register/register.component.ts
@@ -11,7 +11,25 @@ import { UserService } from '../../../../shared/services/user.service';
 })
 export class RegisterComponent implements OnInit{
 
+  formReg: FormGroup;
+
+  constructor(private userService: UserService) {
+    this.formReg = new FormGroup({
+      nombre: new FormControl(),
+      rol: new FormControl('comprador'),
+      apellido: new FormControl(),
+      sexo: new FormControl(),
+      edad: new FormControl(),
+      correo: new FormControl(),
+      imagen: new FormControl(),
+      contraseña: new FormControl(),
+      contraseña2: new FormControl()
+    });
+  }
+
   ngOnInit(): void {
   }
-  
+
+
+
 }

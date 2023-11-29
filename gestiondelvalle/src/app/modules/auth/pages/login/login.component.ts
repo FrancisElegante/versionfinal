c865a78920c1835bb2b183015416b4d9425c18ae
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-28 21:35:20 -0300

    se agrega router

--- gestiondelvalle/src/app/modules/auth/pages/login/login.component.ts
+++ gestiondelvalle/src/app/modules/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',

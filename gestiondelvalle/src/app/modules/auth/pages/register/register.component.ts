26584aa7208c136421f17acaed971827506f3bce
FrancisElegante <nicolasgonza1971@hotmail.com>
2023-11-29 01:25:18 -0300

    se inicia el back del register

--- gestiondelvalle/src/app/modules/auth/pages/register/register.component.ts
+++ gestiondelvalle/src/app/modules/auth/pages/register/register.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../../../shared/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+  ngOnInit(): void {
+  }
+  
 }
